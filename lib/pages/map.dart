d5acfb62f02bb9648331cf9014094d5597311d41
kbuckler <businesstek.denver@gmail.com>
2019-10-04 15:37:55 -0600

    1.0-3 Add FAB and popup for filter

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -7,6 +7,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 //import 'dart:ui' as ui;
 import 'package:pickup_app/services/globals.dart' as globals;
 import 'package:pickup_app/services/auth_provider.dart';
+import 'package:grouped_buttons/grouped_buttons.dart';
 
 class MapPage extends StatefulWidget {
   //MapPage({this.onLoggedOut});
@@ -231,77 +232,90 @@ class MapPageState extends State<MapPage> {
                   },
                   markers: globals.mapmarks),
             ),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: RaisedButton(
-//                  tooltip: 'Menu',
-                  child: const Text('Pin filter for events'),
-                  textColor: Colors.white,
-                  color: Colors.orangeAccent,
-//                  iconSize: 32.00,
-                  elevation: 4.0, //shadow of button
-                  splashColor: Color(0xFF424242),
-//                  icon: Icon(Icons.menu),
-                  onPressed: () => _scaffoldKey.currentState.openDrawer(),
-                ),
-              ),
-            ),
+//             Padding(
+//               padding: const EdgeInsets.all(25.0),
+//               child: Align(
+//                 alignment: Alignment.topCenter,
+//                 child: RaisedButton(
+// //                  tooltip: 'Menu',
+//                   child: const Text('Pin filter for events'),
+//                   textColor: Colors.white,
+//                   color: Colors.orangeAccent,
+// //                  iconSize: 32.00,
+//                   elevation: 4.0, //shadow of button
+//                   splashColor: Color(0xFF424242),
+// //                  icon: Icon(Icons.menu),
+//                   onPressed: () => _scaffoldKey.currentState.openDrawer(),
+//                 ),
+//               ),
+//             ),
           ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            // Add your onPressed code here!
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildAboutDialog(context),
+            );
+          },
+          icon: Icon(Icons.filter_list),
+          label: Text('Filter'),
+          backgroundColor: Colors.orangeAccent,
+          elevation: 20.0,
         ),
       ),
     );
   }
 
   /* Future<void> _goToTheField() async {
-     final GoogleMapController controller = await _controller.future;
-     controller.animateCamera(CameraUpdate.newCameraPosition(_kFolsomField));
-   }*/
+                 final GoogleMapController controller = await _controller.future;
+                 controller.animateCamera(CameraUpdate.newCameraPosition(_kFolsomField));
+               }*/
 
-// Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topLeft,
-//               child: IconButton(
-//                 tooltip: 'Menu',
-//                 color: Colors.orangeAccent,
-//                   iconSize: 32.00,
-//                   icon: Icon(
-//                     Icons.menu
-//                   ),
-//                 onPressed: () => _scaffoldKey.currentState.openDrawer(),
-//               ),
-//             ),
-//           ),
-// //          SizedBox(height: 10.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topCenter,
-//               child: ButtonTheme(
-//                 minWidth: 8.00,
-//                 height: 35.00,
-//                 child: RaisedButton(
-//                   child: const Text('Choose/Create an Activity'),
-//                   textColor: Colors.white,
-//                   color: Colors.orangeAccent,
-//                   elevation: 4.0, //shadow of button
-//                   splashColor: Color(0xFF424242),
-//                   onPressed: (){
-//                     Navigator.of(context).pushNamed('/event_create');
-//                   }
-//                 ),
-//               ),
-//             ),
-//           )
-//             ],
-//           ),
-//         ),
+  // Padding(
+  //             padding: const EdgeInsets.all(25.0),
+  //             child: Align(
+  //               alignment: Alignment.topLeft,
+  //               child: IconButton(
+  //                 tooltip: 'Menu',
+  //                 color: Colors.orangeAccent,
+  //                   iconSize: 32.00,
+  //                   icon: Icon(
+  //                     Icons.menu
+  //                   ),
+  //                 onPressed: () => _scaffoldKey.currentState.openDrawer(),
+  //               ),
+  //             ),
+  //           ),
+  // //          SizedBox(height: 10.0,),
+  //           Padding(
+  //             padding: const EdgeInsets.all(25.0),
+  //             child: Align(
+  //               alignment: Alignment.topCenter,
+  //               child: ButtonTheme(
+  //                 minWidth: 8.00,
+  //                 height: 35.00,
+  //                 child: RaisedButton(
+  //                   child: const Text('Choose/Create an Activity'),
+  //                   textColor: Colors.white,
+  //                   color: Colors.orangeAccent,
+  //                   elevation: 4.0, //shadow of button
+  //                   splashColor: Color(0xFF424242),
+  //                   onPressed: (){
+  //                     Navigator.of(context).pushNamed('/event_create');
+  //                   }
+  //                 ),
+  //               ),
+  //             ),
+  //           )
+  //             ],
+  //           ),
+  //         ),
 
-//       );
-//   }
-// }
+  //       );
+  //   }
+  // }
 
   //               }
   //             ),
@@ -316,56 +330,96 @@ class MapPageState extends State<MapPage> {
 
   //       ))}
 
-//   GoogleMapController mapController;
+  //   GoogleMapController mapController;
 
-//   // void _onMapCreated(GoogleMapController controller){
-//   //   mapController = controller;
-//   // }
-//    Completer<GoogleMapController> _controller = Completer();
+  //   // void _onMapCreated(GoogleMapController controller){
+  //   //   mapController = controller;
+  //   // }
+  //    Completer<GoogleMapController> _controller = Completer();
 
-//  static final CameraPosition _kCuBoulder = CameraPosition(
-//     target: LatLng(40.008131, -105.263782),
-//     zoom: 14.4746,
-//   );
+  //  static final CameraPosition _kCuBoulder = CameraPosition(
+  //     target: LatLng(40.008131, -105.263782),
+  //     zoom: 14.4746,
+  //   );
 
-//   static final CameraPosition _kFolsomField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(40.008131, -105.263782),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //   static final CameraPosition _kFolsomField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(40.008131, -105.263782),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//       static final CameraPosition _kFarrandField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(39.978069, -105.261045),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //       static final CameraPosition _kFarrandField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(39.978069, -105.261045),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//       static final CameraPosition _kKittredgeField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(40.0031513, -105.2624734),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //       static final CameraPosition _kKittredgeField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(40.0031513, -105.2624734),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//        static final CameraPosition _kWilliamsVillageField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(40.0000562, -105.2364027),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //        static final CameraPosition _kWilliamsVillageField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(40.0000562, -105.2364027),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//        static final CameraPosition _kLowerRecField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(40.013933, -105.263218),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //        static final CameraPosition _kLowerRecField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(40.013933, -105.263218),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//        static final CameraPosition _kSportsDomeField = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(40.013933, -105.263218),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
+  //        static final CameraPosition _kSportsDomeField = CameraPosition(
+  //       bearing: 192.8334901395799,
+  //       target: LatLng(40.013933, -105.263218),
+  //       tilt: 59.440717697143555,
+  //       zoom: 19.151926040649414);
 
-//  Future<void> _goToTheField() async {
-//    final GoogleMapController controller = await _controller.future;
-//    controller.animateCamera(CameraUpdate.newCameraPosition(_kFolsomField));
-//  }
+  //  Future<void> _goToTheField() async {
+  //    final GoogleMapController controller = await _controller.future;
+  //    controller.animateCamera(CameraUpdate.newCameraPosition(_kFolsomField));
+  //  }
+}
+
+Widget _buildAboutDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text('Filter Events'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.only(left: 10.0, top: 10.0),
+        ),
+        CheckboxGroup(
+          labels: <String>[
+            "No Filter",
+            "Social",
+            "Cultural and International",
+            "Community Service",
+            "Recreation and Wellness",
+            "Art, Music, and Entertainment",
+            "Happening Soon",
+            "Happening Later",
+          ],
+          onChange: (bool isChecked, String label, int index) =>
+              print("isChecked: $isChecked   label: $label  index: $index"),
+          onSelected: (List<String> checked) =>
+              print("checked: ${checked.toString()}"),
+        ),
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('Filter'),
+      ),
+    ],
+  );
 }
